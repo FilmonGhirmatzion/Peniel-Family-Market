575bd127179c01db175b48530b3d3f246fd9997a
Ghirmatzion, Filmon <ghirmf1@nationwide.com>
2024-10-01 14:48:36 -0500

    proxy added in phone UI

--- phone-ui/src/app/app.component.spec.ts
+++ phone-ui/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'phone-ui'`, () => {
+  it(`should have the 'phone-ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('phone-ui');
